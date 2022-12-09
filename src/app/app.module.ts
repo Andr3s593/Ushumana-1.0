d7a50605b1d434a7631c2e9fe85a8f374127a47f
unknown <ftamay69@gmail.com>
2022-12-08 22:05:46 -0600

    Boletos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TicketsComponent } from './pages/tickets/tickets.component';
+import { TicketComponent } from './pages/ticket/ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketsComponent,
+    TicketComponent
   ],
   imports: [
     BrowserModule,
