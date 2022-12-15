6182d20d8f18424a7281e6c4fd3dbc45323252e1
Andr3s593 <rac.piedra@yavirac.edu.ec>
2022-12-14 22:22:34 -0500

    Nuevos Templates y arreglando errores de diseño

---
+++ src/app/pages/comprarestaurante/comprarestaurante.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comprarestaurante',
+  templateUrl: './comprarestaurante.component.html',
+  styleUrls: ['./comprarestaurante.component.css']
+})
+export class ComprarestauranteComponent { 
+}
