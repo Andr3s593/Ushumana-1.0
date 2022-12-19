f1caf61bb1b05065aaf45acc9b3e9487e9570fea
Andr3s593 <rac.piedra@yavirac.edu.ec>
2022-12-18 21:55:50 -0500

    Model Ticket Implementado

--- src/app/models/ticket.model.ts
+++ src/app/models/ticket.model.ts
@@ -1,19 +1,17 @@
 import { UsuarioModel } from './usuario.model';
 export interface TicketModel {
-    id:number;    
-    precio:number;
-    servicio:string;    
-    detalles:string;
+    id: number;
+    precio: number;
+    servicio: string;
+    detalles: string;
     //qr:BigInteger;//En caso de querer implementar un qr//
 }
-export interface TicketUsuarioModel extends TicketModel{    
-    usuarios:UsuarioModel;
+export interface TicketUsuarioModel extends TicketModel {
+    usuarios: UsuarioModel;
 }
-export interface CreateTicketUsuarioModel  extends Omit < TicketModel, 'id'| 'usuario'> {
+export interface CreateTicketUsuarioModel extends Omit<TicketModel, 'id' | 'usuarios'> {
     usuarios: number;
-    
-    }
-    
-    export interface UpdateTicketUsuarioModel extends Partial< TicketModel> {
-       id: number ;
-    }
+}
+export interface UpdateTicketUsuarioModel extends Partial<TicketModel> {
+    id: number;
+}
--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -2,7 +2,6 @@ export interface UsuarioModel {
     id:number;
     nombres:string;
     apellidos:string;
-    fecha_de_nacimiento:Date;
-    cedula:number;
+    fecha_de_nacimiento:Date;    
     correo:string;
 }
