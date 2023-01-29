439e3db3ab2a9aabcf3570732d4b15ea475e332a
metmaqluin <met.malquin@yavirac.edu.ec>
2023-01-29 10:58:25 -0500

    forulario

---
+++ src/app/pages/formulario/formulario.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comprarestaurante',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class formularioComponent implements OnInit {
+ 
+  ngOnInit(): void {
+  } 
+
+}
