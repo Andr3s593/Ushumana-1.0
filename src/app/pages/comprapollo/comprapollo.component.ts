50875ea208b147fae6e26c79b5749743cc9f185e
unknown <ftamay69@gmail.com>
2023-02-07 20:50:00 -0600

    PolloPedido

--- src/app/pages/comprapollo/comprapollo.component.ts
+++ src/app/pages/comprapollo/comprapollo.component.ts
@@ -1,14 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Subject, Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+import { CreateHistorialCompraDto } from 'src/app/models/historialcompra.model';
+import { HistorialCompraService } from 'src/app/services/historial-compra.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-comprapollo',
   templateUrl: './comprapollo.component.html',
   styleUrls: ['./comprapollo.component.css']
 })
-export class ComprapolloComponent {   
+export class ComprapolloComponent {
+  createPolloPedido: CreateHistorialCompraDto = { imagen: "", nombreplatillo: "", cantidad: 0, precioplatillo: 0 };
+  constructor(private polloHttpService: HistorialCompraService, private router: Router, private elementRef: ElementRef) {
+    this.quantityInput = this.elementRef.nativeElement.querySelector('#input-largo');
+  }
+
+
   quantityControl = new FormControl(0);
   private itemPrice = 7.99;
 
@@ -19,7 +29,6 @@ export class ComprapolloComponent {
     map(quantity => this.itemPrice * quantity),
     startWith(this.itemPrice),
   );
-
   ngOnInit() {
     this.quantityControl.valueChanges.subscribe(value => {
       if (value !== null) {
@@ -27,4 +36,33 @@ export class ComprapolloComponent {
       }
     });
   }
+
+  @ViewChild('quantityInput', { static: true }) quantityInput: ElementRef<HTMLInputElement>;
+
+  createPollo() {
+    const quantity = parseInt(this.quantityInput.nativeElement.value, 10);
+    if (quantity <= 0) {
+    alert("Debe ingresar una cantidad mayor a 0");
+    return;
+    }
+    this.price$.subscribe(price => {
+      const total = quantity * price;
+      const data = {
+        imagen: "https://elmen.pe/wp-content/uploads/2021/10/pollo-en-salsa-de-maracuya.jpg",
+        nombreplatillo: "Pollo al Horno con Salsa de Maracuya",
+        cantidad: quantity,
+        precioplatillo: total,
+      }
+      this.polloHttpService.create(data).pipe().subscribe(response => {
+        console.log(response);      
+        Swal.fire({
+          icon: 'success',
+          title: 'Usuario Registrado',
+          text: 'Usuario creado exitosamente',        
+        })      
+        this.router.navigate(['/login']);        
+      });
+    });
+  } 
+
 }
